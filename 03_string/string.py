fe5773550852cd471e67a1da350755c3cdb03c9d
kimyoungwhan1 <youngwhan12345@naver.com>
2025-07-04 17:46:20 +0900

    collection

--- 03_string/string.py
+++ 03_string/string.py
@@ -23,3 +23,42 @@ print(a[19:-7]) #거꾸로 세는 인덱스
 print(a[::2]) #스텝사용 처음부터 끝까지 인데 스텝이 2번, 첫 인덱스와 끝 인덱스를 를 생략했으니 처음부터 끝까지 인데 스텝을 2를
 #줬으니까 2씩 더하면 되니 0인덱스, 2인덱스, 4인덱스 이런식으로 출력하게 된다.
 print(a[::-1]) #문자열을 거꾸로 뒤집고 싶으면 스텝에 -1을 주면 된다.
+
+#실습
+date = "20250725unny"
+#년도 출력
+print("년도 : ", date[:4])
+#월일 출력
+print("월일: ", date[4:8])
+#날씨 출력
+print("날씨 : ", date[:])
+
+a = "Life is too short, You need Python"
+print(len(a)) #문자열의 길이 알기
+print(a.count("t")) #t가 몇개 들어있는지 출력해줌
+print(a.index("t")) #특정 문자의 인덱스 번호를 바로 알 수 있다. 이거는 3개중에 가장먼저 나온 t의 인덱스가 나올 것이다.
+print(a.index("t", 10, 18))#두 번째나 세번째 인덱스 번호를 알고 싶을때 구간을 설정 할 수 있다.
+##10번부터 18번 까지의 인덱스를 찾기위한 구간설정 (특정문자, 시작인덱스, 끝 인덱스)
+print(a.find("t")) #인덱스랑 똑같은 일을 하는 하나가 더있다.
+#find와 index의 차이점은 index는 해당문자가 없으면 오류를 발생시킨다. find는 -1을 반환한다.
+print(",".join(a)) #문자 합치기 인데 사이 사이에 ,를 다 넣어준다.
+print(a.upper()) #모두 대문자로 바꿔준다.
+print(a.lower()) #모두 소문자로 바꿔준다.
+print(a[0].isupper()) #a의 0번 인덱스는 대문자로 되어있다. 여기서 0번 인덱스의 대문자 여부를 묻는거다.
+print(a[2].islower()) #2번 인덱스의 소문자 여부 출력
+
+b = "       hi      "
+print(b.lstrip()) #왼쪽 공백제거 해준다.
+print(b.rstrip()) #오른쪽 공백제거
+print(b.strip()) #양쪽 공백제거
+print(a.replace("short", "long")) #short를 long로 바꿔준다.
+print(a.replace(" ", "")) #사이사이에 공백들을 제거하기
+print(a.split()) #아무것도 넣지 않으면 공백, 탭, 엔터 기준으로 나눔
+c = "a:b:c:d"
+print(c.split(":"))
+
+#실습
+#먼저 이메일을 입력받아서 변수에 저장, 해당 이메일에서 아이디만 출력하기
+email = input("이메일을 입력하세요 : ")
+print("이메일 아이디 : ", email[:email.index("@")]) #처음부터 @자리까지
+print("이메일 아디디 : ", email.split("@")[0])
--- 04_collection/collection01.py
+++ 04_collection/collection01.py
@@ -1,3 +1,76 @@
-#튜플 언패킹
-a, b, c = colors
-print(a, b, c)
+#List
+from sympy import list2numpy
+
+fruits = ["apple", "banana", "cherry"] #대괄호로 감싸면 문자열 리스트
+numbers = [1, 2, 3, 4, 5] #숫자 리스트
+bools = [True, True, False, True, False] #불리언 리스트
+mixed_list = ["안녕하세요", 42, 3.14, "ptthon", True] #혼합해서 사용이 가능하다.
+
+#요소 접근 (인덱스)
+print(fruits[0]) #apple이 나온다.
+print(fruits[0][0]) #먼저 후르츠0번째인 apple 그리고 apple의 0번째인 a가 출력된다.
+print(numbers[-1])
+
+#요소 변경
+fruits[1] = "blueberry"
+print(fruits)
+
+#요소 추가
+fruits.append("grape") #마지막에 추가됨
+fruits.insert(1, "mango") #원하는 인덱스 자리에 추가한다.
+print(fruits)
+
+#리스트 더하기
+list1 = ["A", "B", "C"]
+list2 = ["D", "E"]
+print(list1 + list2)
+
+#리스트 곱하기
+print(list1 * 3)
+
+#리스트에 여러 요소 추가하기
+list1.extend(list2)
+print(list1)
+
+#요소 삭제
+fruits.remove("cherry") #특정 값을 삭제하기
+fruits.pop() #특정 인덱스로 삭제, 인덱스 생략시 마지막 요소를 삭제 한다. 하나 더 있다.
+del fruits[2]
+print(fruits)
+
+#리스트 길이 구하기
+print(len(fruits))
+
+#리스트 슬라이싱
+print(numbers[1:4]) #2에서 4까지 나온다.
+print(numbers[::-1]) #문자열 뒤집힌다.
+
+#리스트 정렬하기
+numbers.sort() #기본적으로 오름차순으로 정렬된다.
+print(numbers)
+numbers.sort(reverse=True) #내림차순으로 정렬된다.
+print(numbers)
+
+list1.sort(reverse=True)
+print(list1)
+
+kor = ["가", "나", "다"]
+kor.sort(reverse=True) #한글도 가능하다.
+print(kor)
+
+#원본에 영향을 주지않는 정렬
+numbers2 = sorted(numbers) #새롭게 변수에 값을 할당한다.
+
+#리스트 안에 요소가 존재하는지 체크
+print("apple" in fruits) #부정으로 하고 싶으면 not in을 해주면 된다.
+
+#리스트에 요소를 이어 붙이기
+result = "-".join(list1)
+print(result)
+
+
+
+
+
+
+
